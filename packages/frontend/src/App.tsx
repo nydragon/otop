cb11bad9afcb0703422de141e80367ed4b92608f
Paul G <paul_gazeau@korea.ac.kr>
2023-12-12 22:58:46 +0900

    feat: start working on websocket implemetation

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -3,7 +3,7 @@ import "react";
 import "./App.scss";
 
 import { faker } from "@faker-js/faker";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import MeterPie from "./components/atomes/MeterPie";
 import CpuChart from "./components/atomes/CpuChart";
@@ -13,22 +13,57 @@ import MetersRadar from "./components/atomes/MetersRadar";
 import { generateProcess } from "./utils/faker";
 import ProcessModal from "./components/atomes/ProcessModal";
 import { Process } from "./types";
+import useWebSocket from "./hooks/useWebSocket";
 
 export default () => {
+  const { sendMessage, lastMessage, reload, ready } = useWebSocket({
+    url: "ws://localhost:3000/ws",
+  });
+  const [usedMemory, setUsedMemory] = useState(0); // 0
 
-  const [processes, setProcesses] = useState<Process[]>(new Array(10).fill(0).map(() => generateProcess())); // [
+  const [processes, setProcesses] = useState<Process[]>(
+    new Array(10).fill(0).map(() => generateProcess())
+  ); // [
   const [selectedProcess, setSelectedProcess] = useState<Process | null>(null);
 
-  const t = faker.number.int() % 100;
+  useEffect(() => {
+    let interval = setInterval(() => {
+      ready ? sendMessage({ op: 1 }) : reload();
+    }, 5000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [ready]);
+
+  useEffect(() => {
+    if (!lastMessage) return;
+    console.log(lastMessage);
+    if (lastMessage.op === 2) {
+      let memory = lastMessage.d?.memory;
+      if (memory) {
+        let ratio = (memory?.active / memory?.total) * 100;
+        setUsedMemory(ratio);
+      }
+    }
+  }, [lastMessage]);
+
   const nbCpu = (faker.number.int() % 120) + 8;
   const cpusUsage = Array.from(Array(nbCpu).keys()).map((i) => ({
     id: i + 1,
     usage: faker.number.int() % 100,
   }));
 
+  if (!ready) return <div>Connecting...</div>;
+
   return (
     <>
-      {selectedProcess && <ProcessModal close={() => setSelectedProcess(null)} process={selectedProcess} />}
+      {selectedProcess && (
+        <ProcessModal
+          close={() => setSelectedProcess(null)}
+          process={selectedProcess}
+        />
+      )}
       <header>
         <img src="/logo.png" alt="logo" width={75} height={75} />
         <h1>Otop - Dashboard</h1>
@@ -39,11 +74,25 @@ export default () => {
           <MetersRadar />
           <CpuChart cpus={cpusUsage} />
           <div className="meters-pie">
-            {new Array("Memory", "CPU", "Swap", "Network").map((label, index) => (
-              <div key={index} style={{gridArea: `${index >= 2 ? '2' : '1'} / ${(index % 2) + 1} / ${index >= 2 ? '3' : '2'} / ${(index % 2) + 2}`}}>
-                <MeterPie label={label} used={t} width="150px" height="150px" />
-              </div>
-            ))}
+            {new Array("Memory", "CPU", "Swap", "Network").map(
+              (label, index) => (
+                <div
+                  key={index}
+                  style={{
+                    gridArea: `${index >= 2 ? "2" : "1"} / ${
+                      (index % 2) + 1
+                    } / ${index >= 2 ? "3" : "2"} / ${(index % 2) + 2}`,
+                  }}
+                >
+                  <MeterPie
+                    label={label}
+                    used={usedMemory}
+                    width="150px"
+                    height="150px"
+                  />
+                </div>
+              )
+            )}
           </div>
         </div>
         <div className="proc-container">
@@ -63,8 +112,8 @@ export default () => {
           Made with ❤️ by{" "}
           <a href="https://limeal.fr" target="_blank">
             Limeal
-          </a>
-          {" "}and{" "}
+          </a>{" "}
+          and{" "}
           <a href="https://github.com/Nydragon" target="_blank">
             Nydragon
           </a>
